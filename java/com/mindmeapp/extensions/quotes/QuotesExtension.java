6fc1fb133ef48a04b00ff472e782e058387b65aa
Ricardo Lage <riclage@gmail.com>
2014-04-04 09:30:54 +0100

    Adding option to display background image

--- java/com/mindmeapp/extensions/quotes/QuotesExtension.java
+++ java/com/mindmeapp/extensions/quotes/QuotesExtension.java
@@ -17,7 +17,6 @@
 package com.mindmeapp.extensions.quotes;
 
 import java.util.Locale;
-import java.util.Random;
 
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -25,34 +24,44 @@ import android.widget.RemoteViews;
 
 import com.mindmeapp.extensions.ExtensionData;
 import com.mindmeapp.extensions.MindMeExtension;
-import com.mindmeapp.extensions.quotes.R;
 
 public class QuotesExtension extends MindMeExtension {
-    public static final String PREF_SPEAK_BEFORE = "pref_speak_before";
+	public static final String PREF_SPEAK_BEFORE = "pref_speak_before";
+	public static final String PREF_DISPLAY_WALLPAPER = "pref_display_wallpaper";
 
-    @Override
-    protected void onUpdateData(int reason) {
-    	//Get a quote a random to show and speak
-    	Quote q = QuotesDb.getInstance().getRandomQuote();
-    	
-        // Get preference value for text to speak before the quote
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        String name = sp.getString(PREF_SPEAK_BEFORE, getString(R.string.pref_speak_before_default));
+	@Override
+	protected void onUpdateData(int reason) {
+		// Get a quote at random to show and speak
+		Quote q = QuotesDb.getInstance().getRandomQuote();
 
-        //Update RemoteViews layout with the fetched quote
-        RemoteViews rv = new RemoteViews(this.getPackageName(), R.layout.quote_layout);
-        rv.setTextViewText(R.id.quote, q.quote + " (" + q.author + ")");
-        
-        // Publish the extension data update
-        publishUpdate(new ExtensionData()
-                .visible(true)
-                .icon(R.drawable.ic_action_book)
-                .background(R.drawable.background)
-                .statusToDisplay(q.quote + " (" + q.author + ")")
-                .statusToSpeak(q.quote)
-                .languageToSpeak(Locale.US)
-                .contentDescription("Quote from " + q.author + ": " + q.quote)
-                .viewsToDisplay(rv));
-    }
-    
+		// Get preference value for text to speak before the quote
+		// and to display wallpaper
+		SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		String name = sp.getString(PREF_SPEAK_BEFORE,
+				getString(R.string.pref_speak_before_default));
+		boolean displayWallpaper = sp.getBoolean(PREF_DISPLAY_WALLPAPER, false);
+
+		// Update RemoteViews layout with the fetched quote
+		RemoteViews rv = new RemoteViews(this.getPackageName(),
+				R.layout.quote_layout);
+		rv.setTextViewText(R.id.quote, q.quote + " (" + q.author + ")");
+
+		// Sets up the data to publish
+		ExtensionData data = new ExtensionData()
+				.visible(true)
+				.icon(R.drawable.ic_action_book)
+				.statusToDisplay(q.quote + " (" + q.author + ")")
+				.statusToSpeak(name + " " + q.quote)
+				.languageToSpeak(Locale.US)
+				.contentDescription(getString(R.string.content_description, q.author, q.quote))
+				.viewsToDisplay(rv);
+		if (displayWallpaper) {
+			data.background(R.drawable.background);
+		}
+
+		// Publish the extension data update
+		publishUpdate(data);
+	}
+
 }
